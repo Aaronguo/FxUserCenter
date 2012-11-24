fa5210cd5f3d37fa85a87322214756d903525a43
chenzhuojie <117822597@163.com>
2012-11-24 14:36:06 +0800

    基本完善了用户中心
    实现了和主站点的对接，
    后期完善缓存层，还有部分的bug。
    添加相同的异常处理机制。

--- FxUserCenter/FxUserCenter/Controllers/UserCenterController.cs
+++ FxUserCenter/FxUserCenter/Controllers/UserCenterController.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FxCacheService.FxSite;
 
 namespace FxUserCenter.Controllers
 {
+    [Authorize]
     public class UserCenterController : Controller
     {
+        GlobalCache gloablCache;
+        public UserCenterController(GlobalCache gloablCache)
+        {
+            this.gloablCache = gloablCache;
+        }
         //
         // GET: /UserCenter/
 
         public ActionResult About()
         {
-            return View();
+            var about = new AboutModel()
+            {
+                UserCount = gloablCache.UserCount(),
+                UserTodayCount = gloablCache.UserTodayCount(),
+                InfoPublishAllCount = gloablCache.InfoPublishAllCount(),
+                InfoPublishTodayCount = gloablCache.InfoPublishTodayCount(),
+                InfoEndCount = gloablCache.InfoEndCount(),
+                PrivateMessageCount = gloablCache.PrivateMessageCount(User.Identity.Name),
+                PrivateMessageTodayCount = gloablCache.PrivateMessageTodayCount(User.Identity.Name)
+            };
+            return View(about);
         }
 
 
---
+++ FxUserCenter/FxUserCenter/Models/AboutModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FxUserCenter
+{
+    public class AboutModel
+    {
+        public int UserCount { get; set; }
+
+        public int UserTodayCount { get; set; }
+
+        public int InfoPublishAllCount { get; set; }
+
+        public int InfoPublishTodayCount { get; set; }
+
+        public int InfoEndCount { get; set; }
+
+        public int PrivateMessageCount { get; set; }
+
+        public int PrivateMessageTodayCount { get; set; }
+    }
+}
