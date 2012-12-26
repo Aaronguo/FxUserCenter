f146de4f3d339435103e5e26670319df57985855
chenzhuojie <117822597@163.com>
2012-12-27 01:05:27 +0800

    修正用户中心的缓存服务

--- FxUserCenter/FxUserCenter/App_Start/SimpleInjectorInitializer.cs
+++ FxUserCenter/FxUserCenter/App_Start/SimpleInjectorInitializer.cs
@@ -121,7 +121,7 @@ namespace FxUserCenter.App_Start
         {
             container.Register<Fx.Domain.Account.IService.IAccountService, Fx.Domain.Account.UserAccountService>();
             container.Register<Fx.Domain.FxSite.IService.IChannelService, Fx.Domain.FxSite.ChannelService>();
-            container.Register<Fx.Domain.FxSite.IService.IPageAjax, Fx.Domain.FxSite.PublishAjaxService>();
+            container.Register<Fx.Domain.FxSite.IService.ISite, Fx.Domain.FxSite.SiteService>();
 
             //ForSite
             container.Register<Fx.Domain.FxSite.IService.IGoods, Fx.Domain.FxSite.GoodsService>();
